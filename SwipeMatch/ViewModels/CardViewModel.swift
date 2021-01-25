0cd80ebb792b387f4ff65509f0a6a220710f2037
fredlc <fredlefevre@outlook.com>
2021-01-25 12:00:24 -0500

    Created card view model

---
+++ SwipeMatch/ViewModels/CardViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  CardViewModel.swift
+//  SwipeMatch
+//
+//  Created by Fred Lefevre on 2021-01-25.
+//  Copyright © 2021 Fred Lefevre. All rights reserved.
+//
+
+import UIKit
+
+struct CardViewModel {
+    let imageName: String
+    let attributedString: NSAttributedString
+    let textAlignment: NSTextAlignment
+}
