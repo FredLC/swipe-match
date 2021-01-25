ea2cfc9f8152cb539e10fe1f5f2c465d28002474
fredlc <fredlefevre@outlook.com>
2021-01-25 12:47:38 -0500

    Constructed advertiser model, implemented card view model protocol and refactored home controller

---
+++ SwipeMatch/Model/Advertiser.swift
@@ -0,0 +1,22 @@
+//
+//  Advertiser.swift
+//  SwipeMatch
+//
+//  Created by Fred Lefevre on 2021-01-25.
+//  Copyright © 2021 Fred Lefevre. All rights reserved.
+//
+
+import UIKit
+
+struct Advertiser: ProducesCardViewModel {
+    let title: String
+    let brandName: String
+    let posterPhotoName: String
+    
+    func toCardViewModel() -> CardViewModel {
+        let attributedString = NSMutableAttributedString(string: title, attributes: [.font: UIFont.systemFont(ofSize: 34, weight: .heavy)])
+        attributedString.append(NSAttributedString(string: "\n\(brandName)", attributes: [.font: UIFont.systemFont(ofSize: 24, weight: .bold)]))
+        
+        return CardViewModel(imageName: posterPhotoName, attributedString: attributedString, textAlignment: .center)
+    }
+}
