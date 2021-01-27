74d039532753ddfe2d86389a1de495bd0a091019
fredlc <fredlefevre@outlook.com>
2021-01-27 11:44:30 -0500

    Added form rows in settings controller

--- SwipeMatch/Views/SettingsCell.swift
+++ SwipeMatch/Views/SettingsCell.swift
@@ -9,16 +9,36 @@
 import UIKit
 
 class SettingsCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    
+    class SettingsTextField: UITextField {
+        override func textRect(forBounds bounds: CGRect) -> CGRect {
+            return bounds.insetBy(dx: 24, dy: 0)
+        }
+        
+        override func editingRect(forBounds bounds: CGRect) -> CGRect {
+            return bounds.insetBy(dx: 24, dy: 0)
+        }
+        
+        override var intrinsicContentSize: CGSize {
+            return .init(width: 0, height: 50)
+        }
     }
+    
+    let textField: UITextField = {
+        let textField = SettingsTextField()
+        textField.placeholder = "Enter name"
+        return textField
+    }()
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(textField)
+        textField.fillSuperview()
     }
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
