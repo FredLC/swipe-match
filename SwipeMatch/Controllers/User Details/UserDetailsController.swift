a7f787c86616c644b81b7684a7405b1d9aa2850b
fredlc <fredlefevre@outlook.com>
2021-03-15 17:25:16 -0400

    Implemented user details controller and info button

---
+++ SwipeMatch/Controllers/User Details/UserDetailsController.swift
@@ -0,0 +1,18 @@
+//
+//  UserDetailsController.swift
+//  SwipeMatch
+//
+//  Created by Fred Lefevre on 2021-03-15.
+//  Copyright © 2021 Fred Lefevre. All rights reserved.
+//
+
+import UIKit
+
+class UserDetailsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+}
--- SwipeMatch/Views/CardView.swift
+++ SwipeMatch/Views/CardView.swift
@@ -9,7 +9,13 @@
 import UIKit
 import SDWebImage
 
+protocol CardViewDelegate {
+    func didTapMoreInfo()
+}
+
 class CardView: UIView {
+    
+    var delegate: CardViewDelegate?
     
     var cardViewModel: CardViewModel! {
         didSet {
@@ -75,8 +81,19 @@ class CardView: UIView {
     
     fileprivate let moreInfoButton: UIButton = {
         let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "info_icon").withRenderingMode(.alwaysOriginal), for: .normal)
+        button.addTarget(self, action: #selector(handleMoreInfo), for: .touchUpInside)
         return button
     }()
+    
+    @objc fileprivate func handleMoreInfo() {
+        delegate?.didTapMoreInfo()
+//        let rootViewController = UIApplication.shared.keyWindow?.rootViewController
+//        
+//        let userDetailsController = UIViewController()
+//        userDetailsController.view.backgroundColor = .yellow
+//        rootViewController?.present(userDetailsController, animated: true)
+    }
     
     fileprivate func setupLayout() {
         layer.cornerRadius = 10
@@ -94,6 +111,9 @@ class CardView: UIView {
         informationLabel.anchor(top: nil, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor, padding: .init(top: 0, left: 16, bottom: 16, right: 16))
         informationLabel.textColor = .white
         informationLabel.numberOfLines = 0
+        
+        addSubview(moreInfoButton)
+        moreInfoButton.anchor(top: nil, leading: nil, bottom: bottomAnchor, trailing: trailingAnchor, padding: .init(top: 0, left: 0, bottom: 16, right: 16), size: .init(width: 44, height: 44))
     }
     
     fileprivate let barsStackView = UIStackView()
